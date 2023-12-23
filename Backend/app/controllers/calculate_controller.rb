b3c8e09e1b0e9cc571fb92aa8e0d0f4ba34f8063
Reza Mirjahnian <rmirjahanian@gmail.com>
2023-12-23 20:15:33 +0330

    Clean up code

--- Backend/app/controllers/calculate_controller.rb
+++ Backend/app/controllers/calculate_controller.rb
@@ -3,13 +3,10 @@ class CalculateController < ApplicationController
   TUBE_PRICE = 7
   TUBES_IN_UNIT = 4
   FLUORESCENT_UNITS_COUNT = 4
-  CLASSROOM_HOURS_ELAPSED = 9 * 4 * 5 * 15  #todo Determine the precise number of weeks in a calendar year.
-  # CLASSROOM_HOURS_ELAPSED = 300  #todo Determine the precise number of weeks in a calendar year.
+  CLASSROOM_HOURS_ELAPSED = 9 * 4 * 5 * 15 # todo Determine the precise number of weeks in a calendar year.
 
   def cost
-
-    result = CostService.new(CLASSROOM_HOURS_ELAPSED,FLUORESCENT_UNITS_COUNT,TUBES_IN_UNIT,TUBE_PRICE).calculate
-
+    result = CostService.new(CLASSROOM_HOURS_ELAPSED, FLUORESCENT_UNITS_COUNT, TUBES_IN_UNIT, TUBE_PRICE).calculate
     render json: result
 
   end
